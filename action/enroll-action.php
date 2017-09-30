7b1ab75c9681ff4113c785b99cb1c101c2b9660e
Felix <felix@angellecho.com>
2017-09-30 14:48:14 +0800

    no message

--- action/enroll-action.php
+++ action/enroll-action.php
@@ -1,2 +1,25 @@
 <?php
 include_once '../include/lib.php';
+
+$action = post("Action");
+if($action==='clubEnrolll'){
+    $title = post("Title");
+    $time = post("Time");
+    $place = post("Place");
+    $author = post("Author");
+    $position = post("Position");
+    $ability = post("Ability");
+    $positionDesc = post("PositionDesc");
+    $number = post("Number");  
+    $insertArr = [
+        "Time" => $time,
+        "Title" => $time,
+        "Place" => $place,
+        "EnrollPeople" => $author,
+        "AbilityDesc" => $ability,
+        "Position" => $position,
+        "PositionDesc" => $positionDesc,
+        ""
+    
+    ];
+}
