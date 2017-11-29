a69d0779186c194013ac868da0f68c56a1a7256b
Felix <felix@angellecho.com>
2017-11-29 15:14:47 +0800

    no message

--- _cache/template/home.php
+++ _cache/template/home.php
@@ -7,8 +7,7 @@ if ($account===0) {
 $sql = "select LoginTime,IP from Admin22aa order by LoginTime desc limit 0,1";
 $admin = getRowData($sql);
 $count = getSingleData("select count(AdminId) from Admin22aa");
-var_dump($roleType);
-die();
+
 ?>
 <?php include(template("public/header.php"));?>
 <?php include(template("public/meta.php"));?>
